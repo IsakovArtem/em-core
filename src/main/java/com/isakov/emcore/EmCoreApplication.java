ab221622b355178d6012068ef3bdbb456eb86efa
IsakovArtem <isakov_artem@rambler.ru>
2022-08-03 17:35:49 +0300

    Initial commit

---
+++ src/main/java/com/isakov/emcore/EmCoreApplication.java
@@ -0,0 +1,13 @@
+package com.isakov.emcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isakov/emcore/EmCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isakov.emcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
