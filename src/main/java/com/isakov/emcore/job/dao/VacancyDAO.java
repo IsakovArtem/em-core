8c84c0cf86c97d3dbca727e7d7016581ac1c49d7
IsakovArtem <isakov_artem@rambler.ru>
2022-08-12 20:52:43 +0300

    1. named parameters to JDBC 2. non object responses of repository

--- src/main/java/com/isakov/emcore/job/dao/VacancyDAO.java
+++ src/main/java/com/isakov/emcore/job/dao/VacancyDAO.java
@@ -5,30 +5,50 @@ import com.isakov.emcore.job.entity.VacancyEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Repository
 @Component
 @RequiredArgsConstructor
 public class VacancyDAO {
 
     private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedJdbcTemplate;
 
-    public List<VacancyEntity> index() {
+    public List<VacancyEntity> findAll() {
         return jdbcTemplate.query("SELECT * FROM VACANCY", new BeanPropertyRowMapper<>(VacancyEntity.class));
     }
+    public List<Map<String, Object>> findAllInList() {
+        return jdbcTemplate.queryForList("SELECT * FROM VACANCY");
+    }
 
-    public VacancyEntity show(int id) {
-        return jdbcTemplate.query("SELECT * FROM VACANCY WHERE id=?", new BeanPropertyRowMapper<>(VacancyEntity.class),
-                new Object[]{id}).stream().findAny().orElse(null);
+    public List<VacancyEntity> findByIdsAndTitles(List<String> titles, List<Long> ids) {
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("ids", ids);
+        parameters.put("titles", titles);
+
+        return namedJdbcTemplate.query("SELECT * FROM VACANCY WHERE id IN (:ids) AND title IN (:titles)", parameters,
+                new BeanPropertyRowMapper<>(VacancyEntity.class));
+    }
+
+    public VacancyEntity findByIdAndTitle(Long id, String title) {
+        return jdbcTemplate.query("SELECT * FROM VACANCY WHERE id=? OR title=?", new BeanPropertyRowMapper<>(VacancyEntity.class),
+                new Object[]{id, title}).stream().findAny().orElse(null);
     }
 
     public void save(VacancyEntity vacancyEntity) {
 
         SalaryEntity salaryEntity = vacancyEntity.getSalary();
 
-        jdbcTemplate.update("INSERT INTO VACANCY(title, company, salary_from, salary_to, currency, gross, create_date) VALUES(?, ?, ?)",
+        jdbcTemplate.update("INSERT INTO VACANCY(title, company, salary_from, salary_to, currency, gross, create_date) " +
+                                "VALUES(?, ?, ?, ?, ?, ?, ?)",
                 vacancyEntity.getTitle(), vacancyEntity.getCompany(), salaryEntity.getFrom(),
                 salaryEntity.getTo(), salaryEntity.getCurrency(), vacancyEntity.getCreateDate());
     }
--- src/test/java/com/isakov/emcore/job/dao/VacancyDAOTest.java
+++ src/test/java/com/isakov/emcore/job/dao/VacancyDAOTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
 
 @SpringBootTest
 class VacancyDAOTest {
@@ -17,13 +17,37 @@ class VacancyDAOTest {
 
     @Test
     void index() {
-        List<VacancyEntity> vacancyEntityList = vacancyDAO.index();
+        List<VacancyEntity> vacancyEntityList = vacancyDAO.findAll();
         System.out.println(vacancyEntityList);
     }
 
     @Test
-    void show() {
+    void index1() {
+        List<Map<String, Object>> vacancyEntityList = vacancyDAO.findAllInList();
+        System.out.println(vacancyEntityList);
     }
+
+    @Test
+    void findById() {
+
+        List<Long> ids= new ArrayList<>();
+        ids.add(1L);
+        ids.add(3L);
+
+        List<String> titles= new ArrayList<>();
+        titles.add("test");
+        titles.add("Javascript developer");
+
+        List<VacancyEntity> vacancyEntityList = vacancyDAO.findByIdsAndTitles(titles, ids);
+        System.out.println(vacancyEntityList);
+    }
+
+    @Test
+    void findByIdAndTitle() {
+        VacancyEntity vacancyEntity = vacancyDAO.findByIdAndTitle(2L, "test");
+        System.out.println(vacancyEntity);
+    }
+
 
     @Test
     void save() {
