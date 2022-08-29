ef5b78a5ae81b535d8222fe1769c0c60b2eb52e7
IsakovArtem <isakov_artem@rambler.ru>
2022-08-29 14:49:47 +0300

    fix bug classpath:db.migration

--- src/main/java/com/isakov/emcore/job/controllers/VacancyController.java
+++ src/main/java/com/isakov/emcore/job/controllers/VacancyController.java
@@ -4,11 +4,13 @@ import com.isakov.emcore.job.service.SkillService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Map;
 
-@RestController
+@RestController()
 @RequiredArgsConstructor
 public class VacancyController {
 
@@ -18,4 +20,10 @@ public class VacancyController {
     public Map<String, Integer> execute(@PathVariable("vacancy_name") String vacancyName) {
         return skillsService.getSkills(vacancyName);
     }
+
+    @GetMapping(value = "/skills/", produces = "application/json")
+    public Map<String, Integer> executeAlt(@RequestParam(name = "vacancy_name") String vacancyName) {
+        return skillsService.getSkills(vacancyName);
+    }
+
 }
